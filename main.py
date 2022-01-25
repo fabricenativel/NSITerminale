5d748a25b1d819a705b46b1e292bb303edcba594
fabricenativel <fnativel2@ac-reunion.fr>
2022-01-25 16:22:01 +0400

    BNS2022 : 1 à 10 b

--- main.py
+++ main.py
@@ -212,7 +212,7 @@ Vous pouvez télécharger une copie au format pdf du diaporama de synthèse de c
                     correction = f"[Sur Pixees](https://pixees.fr/informatiquelycee/term/ep/s{nums}.html)"+"{target=_blank}"
                 else:
                     correction = f"[{annee}-S{str(nums).zfill(2)}](/NSITerminale/Annales/Corriges/{annee}-S{str(nums).zfill(2)}/)"
-                aff+=f"|{nums}|[Sujet N°{nums}](/officiels/Annales/EP/{annee}/{lf[0]}/{lf[0]}.pdf) | {lf[1]} | {lf[2][:-1]} | [:material-download: Code](/officiels/Annales/EP/{annee}/{lf[0]}/{lf[0]}.py) | {correction} |\n"
+                aff+=f"|{nums}|[Sujet N°{nums}](/NSITerminale/officiels/Annales/EP/{annee}/{lf[0]}/{lf[0]}.pdf) | {lf[1]} | {lf[2][:-1]} | [:material-download: Code](/NSITerminale/officiels/Annales/EP/{annee}/{lf[0]}/{lf[0]}.py) | {correction} |\n"
                 nums+=1
         return aff
     
